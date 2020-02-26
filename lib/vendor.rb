a5569bb349658350e41fcd76cb83f808cfc1a20e
Tyler Porter <tylerpporter@gmail.com>
2020-02-26 10:21:22 -0700

    Complete iteration1

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,17 @@
 class Vendor
+  attr_reader :name, :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = {}
+  end
+
+  def stock(item, quantity)
+    @inventory[item] = quantity
+  end
+
+  def check_stock(item)
+    @inventory[item]
+  end
 
 end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -1,14 +1,29 @@
 require_relative 'test_helper.rb'
 require './lib/vendor.rb'
+require './lib/item.rb'
 
 class VendorTest < Minitest::Test
 
   def setup
-    @vendor = Vendor.new
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
   def test_it_exists
     assert_instance_of Vendor, @vendor
+  end
+
+  def test_it_has_attributes
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+    assert_equal ({}), @vendor.inventory
+  end
+
+  def test_it_can_add_and_check_stock
+    @vendor.stock(@item1, 30)
+    assert_equal 30, @vendor.check_stock(@item1)
+    @vendor.stock(@item2, 12)
+    assert_equal ({@item1=>30, @item2=>12}), @vendor.inventory
   end
 
 end
